9221545f05f9970d567c67e478b83c997ceb6c39
monwar23 <monwar2345@gmail.com>
2024-07-03 02:06:02 +0600

    implement portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
+import Link from 'next/link';
 import React from 'react';
+import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+import { FiGithub } from 'react-icons/fi';
 
 const Footer = () => {
     return (
-        <footer className='-mt-12 relative bottom-0 left-0'>
-        <div className='py-6 text-sm text-center text-white bg-[#21213B]'>
+        <footer className='-mt-12 relative bottom-0 left-0 bg-[#1a1a2e] pt-6'>
+           <ul className="flex justify-center space-x-4">
+                        <li>
+                            <Link href="https://www.facebook.com/monwarhossain.hossain.5" title="Facebook" target='_blank'>
+                                <FaFacebookF className='text-lg hover:text-white text-blue-500' />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="https://www.linkedin.com/in/monwar-hossain23/" title="LinkedIn" target='_blank'>
+                                <FaLinkedinIn className='text-lg hover:text-white text-blue-500' />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="https://github.com/Monwar23" title="GitHub" target='_blank'>
+                                <FiGithub className='text-lg hover:text-white text-blue-500' />
+                            </Link>
+                        </li>
+                    </ul>
+        <div className='pb-6 pt-3 text-sm text-center text-white bg-[#1a1a2e]'>
           © 2024-2025 Monwar Hossain. All rights reserved.
         </div>
       </footer>
