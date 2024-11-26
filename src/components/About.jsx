d578eba72be3594ff6ffefffe5789553332aaea7
monwar23 <monwar2345@gmail.com>
2024-11-26 22:53:58 +0600

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,12 @@ import { Fade } from 'react-awesome-reveal';
 
 const About = () => {
     return (
-       
-            <div className='flex justify-center items-center min-h-screen'>
-            <div className=''>
-            <Fade direction='left'>
-                <h2 className='text-blue-500 text-3xl text-center mt-5 lg:mt-0 font-bold mb-8'>About Me</h2>
+        <div className="flex justify-center items-center min-h-screen px-4">
+            <div className="w-full max-w-2xl">
+                <Fade direction="left">
+                    <h2 className="text-blue-500 text-3xl text-center mt-5 lg:mt-0 font-bold mb-8">About Me</h2>
                 </Fade>
-                <p className='text-white w-4/6 mx-auto'>
+                <p className="text-white text-justify">
                     Hi, I am Monwar Hossain, a passionate and dedicated React Front End Developer. I specialize in creating dynamic web experiences using HTML, CSS, Tailwind CSS, JavaScript, and React. My back-end skills include Node.js, Express.js, and MongoDB, complemented by expertise in version control with Git and GitHub.
                     <br />
                     I am proficient in responsive design, RESTful APIs, and Firebase authentication, ensuring that I deliver high-quality, innovative web solutions tailored to project needs. With a keen eye for detail and a deep understanding of design principles, I strive to bring creativity and functionality together in every project I undertake.
@@ -18,7 +17,6 @@ const About = () => {
                 </p>
             </div>
         </div>
-       
     );
 };
 
